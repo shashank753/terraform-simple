5c9c612bcb5db555f3080627b66df9d580bffb5f
shashank753 <shashanksiddappa@gmail.com>
2024-09-27 12:55:16 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,13 @@
 terraform {
   required_providers {
-    aws = {
+     random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.7.1"
+    }aws = {
       source = "hashicorp/aws"
       version = "5.68.0"
     }
