e97165984a3a6b3c091d2e16ccfe9e25955670bc
shashank753 <shashanksiddappa@gmail.com>
2024-09-28 02:36:40 +0000

    instance

--- instance.tf
+++ instance.tf
@@ -1,6 +1,7 @@
 
 resource "aws_instance" "Raj" {
-  ami           = "ami-08718895af4dfa033"  # Replace with your AMI ID
+  ami           = "ami-08718895af4dfa033"  
   instance_type = "t2.micro"
-  # availability_zone = "ap-south-1c"  # Remove or change this line
+  subnet_id     = aws_subnet.pvt1.id       
+  vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id] 
 }
