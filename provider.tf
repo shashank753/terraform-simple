c08296df5cae6a8a97cacb9b472a91a25ea65090
shashank753 <shashanksiddappa@gmail.com>
2024-09-28 02:40:24 +0000

    all

--- provider.tf
+++ provider.tf
@@ -1,19 +1,29 @@
 terraform {
+  required_version = ">= 0.12"
   required_providers {
-     random = {
+    random = {
       source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">=2.7.1"
-    }aws = {
-      source = "hashicorp/aws"
-      version = "5.68.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.68.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1.0"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.2.0"
     }
   }
-}
-
-provider "aws" {
-  region = "ap-south-1"
 }
