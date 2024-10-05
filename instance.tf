32971eea5d71b3ad38da5594d16aadbc46df6378
shashank753 <shashanksiddappa@gmail.com>
2024-10-05 07:03:00 +0530

    Delete instance.tf

--- instance.tf
+++
@@ -1,7 +0,0 @@
-
-resource "aws_instance" "Raj" {
-  ami           = "ami-08718895af4dfa033"  
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.pvt1.id       
-  vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id] 
-}
